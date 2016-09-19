aba88e0cc3c1146ce01bbde4467950a5b3d81d70
Lucas Allmon <lucaskallmon@gmail.com>
2016-09-18 17:13:41 -0700

    Add basic app components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import animate from 'animate.css'; //eslint-disable-line
+import tachyons from 'tachyons'; //eslint-disable-line
+import Header from './Header';
+import Venue from './VenuePreview';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { appLoading: true, venues: [] }
+  }
+
+  componentDidMount() {
+    axios.get('http://api-staging.theculturelist.org/v1/venues')
+      .then((response) => {
+        this.setState({ appLoading: false, venues: response.data});
+      })
+      .catch((error) => {
+        console.log(error);
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    if (this.state.loading === false) {
+      return (
+        <div className="application">
+          <Header />
+          <section className="content-body">
+            {this.state.venues.map((venue) => (
+              <Venue
+                key={venue.id}
+                name={venue.name}
+                imageUrl={venue.media[0].widescreen}
+                address={venue.address.formatted_address}
+                distance={venue.location.latitude}
+              />
+            ))}
+
+          </section>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      )
+    }
+
+    return (<h1>Loading</h1>)
   }
 }
 
---
+++ src/VenuePreview.js
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes as PT} from 'react';
+const cloudinaryPrefix = "http://res.cloudinary.com/theculturelist/image/upload/q_auto";
+
+class Venue extends Component {
+  static propTypes = {
+    name: PT.string.isRequired,
+    imageUrl: PT.string.isRequired,
+    address: PT.string.isRequired,
+    distance: PT.number.isRequired,
+  };
+
+  static defaultProps = {
+    name: "Coming Soon",
+    imageUrl: "/v1455314704/Placeholder-16x9_zpm4cq.jpg",
+    address: "Your City",
+    distance: "Close",
+  };
+
+  render() {
+    return (
+      <article className="pointer cf ba b--black-30 mv3 mh2 br2 fr w-100 w-third-ns animated slideInUp">
+        <div className="pv2 ph2">
+           <h1 className="f6 ttu dark-gray">{this.props.name}</h1>
+        </div>
+        <img src={cloudinaryPrefix + this.props.imageUrl} className="grow w-100 db" alt={this.props.title} />
+        <div className="pa3">
+          <a href="#" className="link f6">{this.props.address}</a>
+          <small className="gray db pv2">Hours Today: </small>
+          <small className="gray db pv2">Distance Away - {this.props.distance}</small>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default Venue;
