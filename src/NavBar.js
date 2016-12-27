fd1f3a6f7ec8d20cc37f53c5c2128a19c1cefc9d
Lucas K Allmon <lucaskallmon@gmail.com>
2016-12-26 23:39:30 -0500

    fix(navbar): render fix for  main browsers (#29)

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,21 +4,19 @@ import CloudinaryImage from './CloudinaryImage'
 
 const NavBar = props => (
   <div className='nav-bar'>
-    <header className='bg-dark-gray fixed shadow-1 z-2 w-100'>
-      <nav className='h3 flex items-center mh2'>
-        <div className='absolute w-100 flex h3 items-center justify-center'>
-          <Link to='/'>
-            <CloudinaryImage
-              alt='The Culture List Icon'
-              className='h2 w2 pointer'
-              src='icon_yh2ghw.svg'
-            />
-          </Link>
-        </div>
-        <div className="flex dn-l z-3">
-          {props.children}
-        </div>
-      </nav>
+    <header className='bg-dark-gray fixed h3 shadow-1 w-100 z-2 flex items-center ph2'>
+      <div className='absolute w-100 flex h3 items-center justify-center top-0'>
+        <Link to='/'>
+          <CloudinaryImage
+            alt='The Culture List Icon'
+            className='h2 w2 pointer'
+            src='icon_yh2ghw.svg'
+          />
+        </Link>
+      </div>
+      <div className="flex dn-l z-3">
+        {props.children}
+      </div>
     </header>
   </div>
 )
