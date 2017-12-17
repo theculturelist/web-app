f9088f7d92a312adc101baecf78fba459515f8c5
Lucas K Allmon <lucaskallmon@gmail.com>
2017-12-16 16:40:15 -0800

    chore(dependencies): update

--- src/InitScreen.js
+++ src/InitScreen.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactSpinner from 'react-spinjs'
 import { random } from 'lodash'
 import CloudinaryImage from './CloudinaryImage'
 
@@ -20,7 +19,6 @@ const InitScreen = (props) => (
       alt={'logo'}
       transform={{quality: 'auto'}}
     />
-    <ReactSpinner width={100} height={100} color="white" />
     <h1 className="f3 f2-l fw1 pv4 tc">{ Messages[random(Messages.length - 1)] }...</h1>
     <h2 className="f5 f4-l fw1 pv4 tc">{'To give you the best experience.'}</h2>
   </section>
